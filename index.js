42e0bf700bee5930ee60b48a21cd622a3975d919
James E. Marca <jmarca@translab.its.uci.edu>
2013-02-07 12:55:18 -0800

    added a function for osm links, for simple web app

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 exports.carb_areas  = require('./lib/carb_areas').carb_areas
 exports.detectors   = require('./lib/detectors').detectors
-
+exports.osm_links   = require('./lib/osm_links').osm_links
