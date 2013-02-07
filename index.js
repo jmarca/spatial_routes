a32b95a362a7c44b1c39e23007bf5653f5949540
James E. Marca <jmarca@translab.its.uci.edu>
2013-02-07 14:24:45 -0800

    added osm links, osm topo links functions, w/ tests
    modified:   index.js
    modified:   lib/osm_links.js
    modified:   package.json
    new file:   test/osm_links_test.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.carb_areas  = require('./lib/carb_areas').carb_areas
 exports.detectors   = require('./lib/detectors').detectors
 exports.osm_links   = require('./lib/osm_links').osm_links
+exports.osm_topojson   = require('./lib/osm_links').osm_topojson
