d4670ef49602b9dedb42b445123cff25236f90f9
James E. Marca <james@activimetrics.com>
2016-02-24 20:09:55 -0800

    automated fixing of semicolons and spacing

--- test/test_murb_links.js
+++ test/test_murb_links.js
@@ -4,8 +4,8 @@
 
 var should = require('should')
 
-var request = require('request');
-var _ = require('lodash');
+var request = require('request')
+var _ = require('lodash')
 var murb_links = require('../lib/murb_links').murb_links
 var murb_topojson = require('../lib/murb_links').murb_topojson
 var detectors  = require('../lib/detectors').detectors
@@ -13,11 +13,11 @@ var detectors  = require('../lib/detectors').detectors
 var http = require('http')
 var express = require('express')
 
-var env = process.env;
-var puser = process.env.PSQL_USER ;
-var ppass = process.env.PSQL_PASS ;
-var phost = process.env.PSQL_HOST ;
-var pport = process.env.PSQL_PORT || 5432;
+var env = process.env
+var puser = process.env.PSQL_USER 
+var ppass = process.env.PSQL_PASS 
+var phost = process.env.PSQL_HOST 
+var pport = process.env.PSQL_PORT || 5432
 
 
 var testhost = env.SHAPES_TEST_HOST || '127.0.0.1'
@@ -26,7 +26,7 @@ var testport = env.SHAPES_TEST_PORT || 3000
 
 describe ('murb links service', function(){
 
-    var app,server;
+    var app,server
 
     before(
         function(done){
@@ -47,47 +47,47 @@ describe ('murb links service', function(){
     it('should spit out geojson'
       ,function(done){
            // load the service for vds shape data
-           request({'url':'http://'+ testhost +':'+testport+'/detectors/murb/2007/11/353/820.json'
+          request({'url':'http://'+ testhost +':'+testport+'/detectors/murb/2007/11/353/820.json'
                    ,'headers':{'accept':'application/json'}
                    ,'followRedirect':true}
                   ,function(e,r,b){
-                       if(e) return done(e)
-                       r.statusCode.should.equal(200)
-                       should.exist(b)
-                       var c = JSON.parse(b)
-                           c.should.have.property('type','FeatureCollection')
-                           c.should.have.property('features')
-                       c.features.should.have.property('length')
-                       c.features.length.should.eql(102)
-                       return done()
-                   })
-       })
+               if(e) return done(e)
+               r.statusCode.should.equal(200)
+               should.exist(b)
+               var c = JSON.parse(b)
+               c.should.have.property('type','FeatureCollection')
+               c.should.have.property('features')
+               c.features.should.have.property('length')
+               c.features.length.should.eql(102)
+               return done()
+           })
+      })
     it('should spit out topojson, not geojson'
       ,function(done){
            // load the service for vds shape data
-           request({'url':'http://'+ testhost +':'+testport+'/detectors/murb_topo/2008/11/353/820.json'
+          request({'url':'http://'+ testhost +':'+testport+'/detectors/murb_topo/2008/11/353/820.json'
                    ,'headers':{'accept':'application/json'}
                    ,'followRedirect':true}
                   ,function(e,r,b){
-                       if(e) return done(e)
-                       r.statusCode.should.equal(200)
-                       should.exist(b)
-                       var c = JSON.parse(b)
-                       c.should.have.property('type','Topology')
-                       c.should.have.property('arcs')
-                       c.arcs.should.have.property('length')
-                       c.arcs.length.should.eql(152)
+               if(e) return done(e)
+               r.statusCode.should.equal(200)
+               should.exist(b)
+               var c = JSON.parse(b)
+               c.should.have.property('type','Topology')
+               c.should.have.property('arcs')
+               c.arcs.should.have.property('length')
+               c.arcs.length.should.eql(152)
 
-                       c.should.have.property('objects')
-                       var topo_objects = c.objects
-                       _.each(topo_objects
+               c.should.have.property('objects')
+               var topo_objects = c.objects
+               _.each(topo_objects
                              ,function(obj){
-                                  obj.should.have.property('type','LineString')
-                                  obj.should.have.property('id')
-                                  obj.id.should.match(/^\d+_(north|south|east|west)$/)
-                              });
+                                 obj.should.have.property('type','LineString')
+                                 obj.should.have.property('id')
+                                 obj.id.should.match(/^\d+_(north|south|east|west)$/)
+                             })
 
-                       return done()
-                   })
-       })
+               return done()
+           })
+      })
 })
